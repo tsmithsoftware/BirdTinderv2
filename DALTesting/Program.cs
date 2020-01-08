ec8b52d21fc294305f045c9690b2419fd5ea51ba
Smith <tijak@hotmail.com>
2020-01-08 16:22:51 +0000

    Basic API talking to the DAL

--- DALTesting/Program.cs
+++ DALTesting/Program.cs
@@ -10,14 +10,24 @@ namespace DALTesting
         private static ModelContext modelContext = new ModelContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var repo = new SystemUserRepo(modelContext);
-            var user = new SystemUser();
-            user.Id = repo.GetSize();
-            user.FirstName = "John";
-            user.LastName = "Mcclane";
-            user.Password = HashProvider.sha256("password");
-            repo.Insert(user);
+            /** var repo = new SystemUserRepo(modelContext);
+             var user = new SystemUser();
+             user.Id = repo.GetSize();
+             user.FirstName = "John";
+             user.LastName = "Mcclane";
+             user.Password = HashProvider.sha256("password");
+             repo.Insert(user);
+
+             var allUsers = repo.GetAll();
+             var count = allUsers.Count; **/
+
+            var repo = new BirdUserRepo(modelContext);
+            var bird = new BirdUser();
+            bird.UserName = "Bird";
+            bird.UserId = repo.GetSize();
+            bird.UserBio = "Some Great Crested Tit";
+            bird.UserImageUri = "someuri";
+            repo.Insert(bird);
         }
     }
 }
